3b14425eeec85e9a216cc6c23e54b0b8b51d903b
Aslam <aslamidrs@gmail.com>
2021-10-21 16:52:02 +0530

    blog

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyBnhgdczxRfn9aCH0NJz0tzI1aOnR_E-Dw",
+    authDomain: "my-blog-a3adb.firebaseapp.com",
+    projectId: "my-blog-a3adb",
+    storageBucket: "my-blog-a3adb.appspot.com",
+    messagingSenderId: "537552324296",
+    appId: "1:537552324296:web:88caa00997d01d1e8d07fd",
+    measurementId: "G-HGF1L6M6GC"
+  };
